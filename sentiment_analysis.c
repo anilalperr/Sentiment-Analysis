04035153e467d69b9243f43b5473436e45712835
Okada <okadahar@peano.cs.grinnell.edu>
2021-12-04 16:57:05 -0600

    get txt attempt

--- sentiment_analysis.c
+++ sentiment_analysis.c
@@ -1,7 +1,9 @@
-#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 //Helpers to Implement
 //*isReadable (see line 34)
@@ -10,36 +12,96 @@
 typedef struct node
 {
     char *txt_name;
-    node_t *next;
+    struct node *next;
 } node_t;
 
 // Linked list of txt file
 typedef struct txt_lst 
 {
     node_t *first_txt;
-}
+}txt_lst_t;
 
 // Find all txt file in the directory
-void getTxtFiles(const char* directory_name) {
+void getTxtFiles(const char* directory_name, txt_lst_t* txtList) {
+    printf("entered get txt file\n");
 
     // variables to open a directory and access the contents of this directory
-    DIR* directory;
-    struct dirent* dirContent;
+  
+    struct dirent* entry;
+    struct stat status;
+    int isReadable = 0;
+
+
+    DIR* directory = opendir(directory_name);
 
     // open the directory
-    if ((directory = opendir(directory_name)) != NULL) {
+    if (directory != NULL) {
+        printf("opened directory\n");
         //  for all the files and directories within this directory
-        while ((dirContent = readdir(directory)) != NULL) {
-            // create a struct to store the information about file
-            struct stat fileInfo;
+        while ((entry = readdir(directory)) != NULL) {
+            printf("entered the while loop\n");
 
-            // access the path to the file
-            char* subDirectory = getSubDirectory(directory_name, dirContent->d_name);
+            //Create path of directory
+            char path[strlen(directory_name)+strlen(entry->d_name)+2];
+            path[0] = '\0';
+            strcat(path,directory_name);
+            strcat(path,"/");
+            strcat(path,entry->d_name);
+            printf("entry name is %s\n", entry->d_name);
+
+            //Check for status of the file
+            if (stat(path,&status) == -1) {
+                printf("%s \n",path);
+                perror("Failed");
+            } 
+
+
+            // Check if the file is a txt file
+            if (strstr(".txt", entry->d_name) != NULL) {
+                //put in linked list
+                node_t *newFile = (node_t *)malloc(sizeof(node_t));
+                newFile->txt_name = entry->d_name;
+
+                //If txt list is empty (first element)
+                if (txtList->first_txt == NULL) {
+
+                    newFile->next = txtList->first_txt;
+                    txtList->first_txt->next = newFile;
+                    newFile->next = NULL;
+                } else { //if there is at least one file in the list, 
+                    node_t *current = txtList->first_txt;
+
+                    while (current->next != NULL) {
+                        current = current->next;
+                    }
+
+                    newFile->next = current->next;
+                    current->next = newFile;
+                }
+
+                printf("%s\n",entry->d_name);
+
+
+            }
+
+               
+            // create a struct to store the information about file
+
+           // access the path to the file
+            //char* subDirectory = getSubDirectory(directory_name, dirContent->d_name);
 
             // access the file permissions [IMPLEMENT THE HELPER!!!!!!]
-            boolean readable = isReadable(fileInfo.st_mode, subDirectory);
+            //boolean readable = isReadable(fileInfo.st_mode, subDirectory);
 
             // Find all txt files, put them in linked list
+            if (status.st_mode == S_IRUSR) {
+                isReadable = 1;
+            } else {
+                isReadable = 0;
+            }
+            //// Find txt file
+
+            /// check readability
 
 
         }
@@ -47,6 +109,7 @@ void getTxtFiles(const char* directory_name) {
     }
     else {  // if no such directory exists print a message
         fprintf(stderr, "Failed to open directory: no such file or directory\n");
+        exit(EXIT_FAILURE);
     }
 
 }
@@ -61,6 +124,13 @@ int main(int argc, char** argv) {
 
     // access the name of directory
     const char* directory_name = argv[1];
+
+    //create a linked list 
+    txt_lst_t *txtList; 
+    txtList->first_txt = NULL;
+
+    getTxtFiles(directory_name, txtList);
+
 
     
 
