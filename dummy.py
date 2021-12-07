9ab325a1bb2d89aaf4de9b5f4cf36a92b8b75090
harooks <haruko.flipsy.krb@gmail.com>
2021-12-06 20:49:21 -0600

    added main to python file

--- dummy.py
+++ dummy.py
@@ -1,5 +1,15 @@
+import os
+import re
+import sys
+
 def countWords(txtfile):
   file = open(txtfile, "rt")
   data = file.read()
   words = data.split()
   print('Number of words in text file :', len(words))
+
+def main():
+  if len(sys.argv) != 2:
+    sys.exit("Usage: python dummy.py [txtfilename]")
+
+  countWords(sys.argv[1])
